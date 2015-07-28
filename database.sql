d997c0c0714beecfbdd503682e09a9889389037d
Phil Pastorek <contact@hirethisdeveloper.com>
2015-07-27 23:12:54 -0400

    updated database sql file

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `adt_alerts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `dateCreated` datetime DEFAULT NULL,
+  `dateCreated` varchar(64) DEFAULT NULL,
   `alert` varchar(32) DEFAULT NULL,
   `zone` varchar(32) DEFAULT NULL,
   `location` varchar(64) DEFAULT NULL,
   `zone_title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=64 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
